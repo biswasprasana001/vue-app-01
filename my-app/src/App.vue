e7de08944971df715d62e60c534169a0fabbe057
biswasprasana001 <biswasprasana004@gmail.com>
2024-06-15 23:25:38 +0530

    Implemented Vue Challenges 01

---
+++ my-app/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <CounterState />
+  <SampleRoute />
+</template>
+
+<script>
+import CounterState from './components/CounterState.vue';
+import SampleRoute from './components/SampleRoutes.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CounterState,
+    SampleRoute
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my-app/src/components/Routes/About.vue
@@ -0,0 +1,14 @@
+<!-- About.vue -->
+<template>
+    <div>
+      <h1>About Page</h1>
+      <p>This page tells you all about our Vue app.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'AboutComponent'
+  };
+  </script>
+  
